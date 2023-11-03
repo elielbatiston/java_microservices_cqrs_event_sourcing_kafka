9ac8c2fbea429db09f53b2d3baba014d1f71b127
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-11-03 11:27:21 -0300

    add aggregates

---
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/domain/AccountAggregate.java
@@ -0,0 +1,86 @@
+package com.techbank.account.cmd.domain;
+
+import com.techbank.account.cmd.api.commands.OpenAccountCommand;
+import com.techbank.account.common.dto.AccountType;
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawEvent;
+import com.techbank.cqrs.core.domain.AggregateRoot;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+public class AccountAggregate extends AggregateRoot {
+	private Boolean active;
+	private double balance;
+
+	public AccountAggregate(final OpenAccountCommand command) {
+		raiseEvent(AccountOpenedEvent.builder()
+			.id(command.getId())
+			.accountHolder(command.getAccountHolder())
+			.createdDate(new Date())
+			.accountType(command.getAccountType())
+			.openingBalance(command.getOpeningBalance())
+			.build()
+		);
+	}
+
+	public void apply(final AccountOpenedEvent event) {
+		this.id = event.getId();
+		this.active = true;
+		this.balance = event.getOpeningBalance();
+	}
+
+	public void depositFunds(final double amount) {
+		if (!this.active) {
+			throw new IllegalStateException("Funds cannot be depoisted into a closed account!");
+		}
+		if (amount <= 0) {
+			throw new IllegalStateException("The deposit amount be greater than 0!");
+		}
+		raiseEvent(FundsDepositedEvent.builder()
+			.id(this.id)
+			.amount(amount)
+			.build()
+		);
+	}
+
+	public void apply(final FundsDepositedEvent event) {
+		this.id = event.getId();
+		this.active = true;
+		this.balance = event.getAmount();
+	}
+
+	public void withdrawFunds(final double amount) {
+		if (!this.active) {
+			throw new IllegalStateException("Funds cannot be withdrawn from a closed account!");
+		}
+		raiseEvent(FundsWithdrawEvent.builder()
+			.id(this.id)
+			.amount(amount)
+			.build()
+		);
+	}
+
+	public void apply(final FundsWithdrawEvent event) {
+		this.id = event.getId();
+		this.balance = event.getAmount();
+	}
+
+	public void closeAccount() {
+		if (!this.active) {
+			throw new IllegalStateException("The bank account has already been closed!");
+		}
+		raiseEvent(AccountClosedEvent.builder()
+			.id(this.id)
+			.build()
+		);
+	}
+
+	public void apply(final AccountClosedEvent event) {
+		this.id = event.getId();
+		this.active = false;
+	}
+}
---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/domain/AggregateRoot.java
@@ -0,0 +1,59 @@
+package com.techbank.cqrs.core.domain;
+
+import com.techbank.cqrs.core.events.BaseEvent;
+import lombok.Getter;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class AggregateRoot {
+	@Getter
+	protected String id;
+	@Getter
+	private int version = -1;
+
+	private final List<BaseEvent> changes = new ArrayList<>();
+	private final Logger logger = Logger.getLogger(AggregateRoot.class.getName());
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	public List<BaseEvent> getUncommittedChanges() {
+		return this.changes;
+	}
+
+	public void markChangeAsCommitted() {
+		this.changes.clear();
+	}
+
+	protected void applyChanges(final BaseEvent event, Boolean isNewEvent) {
+		try {
+			var method = getClass().getDeclaredMethod("apply", event.getClass());
+			method.setAccessible(true);
+			method.invoke(this, event);
+		} catch (final NoSuchMethodException e) {
+			logger.log(
+				Level.WARNING,
+				MessageFormat.format("The apply method was not found in the aggregate for {0}", event.getClass().getName())
+			);
+		} catch (final Exception e) {
+			logger.log(Level.SEVERE, "Error applying event to aggregate", e);
+		} finally {
+			if (isNewEvent) {
+				changes.add(event);
+			}
+		}
+	}
+
+	public void raiseEvent(final BaseEvent event) {
+		applyChanges(event, true);
+	}
+
+	public void replayEvents(final Iterable<BaseEvent> events) {
+		events.forEach(event -> applyChanges(event, false));
+	}
+}
